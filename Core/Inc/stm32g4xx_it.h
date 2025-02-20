9bc0b01c7b7bb20b6b023953cb8ff41566b733c0
Haoqi Liu <2675794963@qq.con>
2025-02-21 04:54:22 +0800

    成功接收CAN报文
    以FOC_PM4310为基础,测试CAN功能,实现接收C610电调报文.

--- Core/Inc/stm32g4xx_it.h
+++ Core/Inc/stm32g4xx_it.h
@@ -54,6 +54,8 @@ void UsageFault_Handler(void);
 void DebugMon_Handler(void);
 void SysTick_Handler(void);
 void ADC1_2_IRQHandler(void);
+void FDCAN1_IT0_IRQHandler(void);
+void FDCAN1_IT1_IRQHandler(void);
 void TIM6_DAC_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
